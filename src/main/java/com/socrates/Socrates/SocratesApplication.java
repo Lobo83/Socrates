f39f78e022b7de6ba1909c9823480b46a1e9cf85
Jose Luis Anton Bueso <jlanton@textplus.com>
2019-06-21 19:10:54 +0200

    primer commit

---
+++ src/main/java/com/socrates/Socrates/SocratesApplication.java
@@ -0,0 +1,13 @@
+package com.socrates.Socrates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SocratesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SocratesApplication.class, args);
+	}
+
+}
